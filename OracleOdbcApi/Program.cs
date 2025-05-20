a472f895519ef72de4950622fcde84494a586160
t-sugimoto-zc <t-sugimoto_zc@mx2.toyotasystems.com>
2025-05-20 13:39:46 +0900

    Change cs

--- OracleOdbcApi/Program.cs
+++ OracleOdbcApi/Program.cs
@@ -1,10 +1,21 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "OracleOdbcApi",
+        Version = "v1"
+    });
+});
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
